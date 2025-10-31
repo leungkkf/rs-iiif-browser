df9a8326eae3eb10d5c78c6b607ab0960b72f396
leungkkf <leungkkf@gmail.com>
2025-10-31 16:10:13 +0000

    use local to keep last mouse pos

--- src/mouse_input.rs
+++ src/mouse_input.rs
@@ -1,14 +1,10 @@
 use crate::{AppState, tile::TileModState, tiled_image::TiledImage};
 use bevy::{input::mouse::AccumulatedMouseScroll, prelude::*, window::PrimaryWindow};
 
-#[derive(Component)]
-pub(crate) struct MousePosition(Vec2);
-
 pub(crate) fn handle_mouse_input(
-    mut commands: Commands,
     camera: Single<(&Camera, &GlobalTransform, &mut Transform, &mut Projection), With<Camera2d>>,
-    app_state_query: Single<(Entity, &mut AppState), With<AppState>>,
-    stored_mouse_pos: Query<&MousePosition>,
+    mut app_state: Single<&mut AppState>,
+    mut stored_mouse_pos: Local<Option<Vec2>>,
     mouse_wheel_input: Res<AccumulatedMouseScroll>,
     mouse: Res<ButtonInput<MouseButton>>,
     window: Single<&Window, With<PrimaryWindow>>,
@@ -21,7 +17,6 @@ pub(crate) fn handle_mouse_input(
         return;
     };
 
-    let (app_state_entity, mut app_state) = app_state_query.into_inner();
     let mut current_mouse_pos = Vec2::ZERO;
     let is_pressed = mouse.pressed(MouseButton::Left);
 
@@ -37,27 +32,21 @@ pub(crate) fn handle_mouse_input(
         }
     }
 
-    if stored_mouse_pos.is_empty() {
-        if mouse.just_pressed(MouseButton::Left) {
-            commands
-                .entity(app_state_entity)
-                .insert(MousePosition(current_mouse_pos));
-        }
-    } else {
+    if let Some(mouse_pos) = *stored_mouse_pos {
         if is_pressed {
-            let mouse_pos = stored_mouse_pos.single().expect("should have one item");
-
             transform.translation += Vec3::new(
-                mouse_pos.0.x - current_mouse_pos.x,
-                mouse_pos.0.y - current_mouse_pos.y,
+                mouse_pos.x - current_mouse_pos.x,
+                mouse_pos.y - current_mouse_pos.y,
                 0.0,
             );
         }
 
         if mouse.just_released(MouseButton::Left) {
-            commands.entity(app_state_entity).remove::<MousePosition>();
+            *stored_mouse_pos = None;
             tile_mod_state.invalidate();
         }
+    } else if mouse.just_pressed(MouseButton::Left) {
+        *stored_mouse_pos = Some(current_mouse_pos);
     }
 
     let delta_zoom = 1.0 - mouse_wheel_input.delta.y * 0.1;
